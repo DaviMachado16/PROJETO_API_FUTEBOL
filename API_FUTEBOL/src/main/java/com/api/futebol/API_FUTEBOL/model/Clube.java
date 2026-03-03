d865ad77397b3f6d9f525f9ea61aa964e698a575
DaviMachado16 <davimachado.al16@gmail.com>
2026-03-03 18:26:25 -0300

    Atualizar o Clube.java
    Melhoria de codigo, deletei os getters e setters, e adicionei eles como anotação

--- API_FUTEBOL/src/main/java/com/api/futebol/API_FUTEBOL/model/Clube.java
+++ API_FUTEBOL/src/main/java/com/api/futebol/API_FUTEBOL/model/Clube.java
@@ -1,7 +1,11 @@
-package com.api.futebol.API_FUTEBOL.model;
+já package com.api.futebol.API_FUTEBOL.model;
 
 import jakarta.persistence.*;
 
+@NoArgsConstructor
+@AllArgdConstructor
+@Getters
+@Setters
 @Entity
 @Table(name="clube")
 public class Clube {
@@ -16,28 +20,4 @@ public class Clube {
 
     @Column(name="pais", length = 100, nullable = false)
     private String pais;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getPais() {
-        return pais;
-    }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
 }
